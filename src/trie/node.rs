b61be896073a130fd671e43045bc321d248c4b91
hoggetaylor <u0900375@utah.edu>
2016-07-01 19:55:00 +0000

    Remove Box from trie

--- src/trie/node.rs
+++ src/trie/node.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Node {
-    pub nodes: HashMap<char, Box<Node>>,
+    pub nodes: HashMap<char, Node>,
     pub is_word: bool
 }
 
@@ -17,11 +17,11 @@ impl Node {
     /// creates a new one and returns a reference to it.
     /// If one already exists, returns a reference to the existing node.
     pub fn add_node(&mut self, ch: char) -> &mut Node {
-        self.nodes.entry(ch).or_insert(Box::new(Node::new()))
+        self.nodes.entry(ch).or_insert(Node::new())
     }
 
     /// Returns a reference to the node held at the given letter.
     pub fn get_node<'a>(&'a self, ch: char) -> Option<&'a Node> {
-        self.nodes.get(&ch).map(|node| &**node)
+        self.nodes.get(&ch)
     }
 }
