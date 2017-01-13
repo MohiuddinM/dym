3f1ac0e8ee2d437b6537c772cd09272656cdecb1
MohiuddinM <muhammad.mohiuddin@live.com>
2017-01-13 22:13:58 +0500

    Faster hash functions and string conversion

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
+extern crate fnv;
+extern crate strsim;
+
+
 mod trie;
 mod pattern;
 
 use self::trie::Trie;
 use std::collections::HashSet;
+//use fnv::FnvHashMap;
+use fnv::FnvHashSet;
+use fnv::FnvBuildHasher;
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Lexicon {
@@ -17,6 +24,9 @@ impl Lexicon {
             words: Trie::new(),
         }
     }
+
+    //pub fn new_from_file(path: &str) {  
+    //}
 
     /// Inserts a copy of the given word into the Lexicon
     /// after converting it to lowercase and trimming whitespace
@@ -39,6 +49,13 @@ impl Lexicon {
         self.words.contains(&word)
     }
 
+    pub fn best_match_for(&self, word: &str) -> String {
+        let corrections = self.corrections_for(&word);
+        let (_, correct_word) = corrections.iter().map(|test_word| (strsim::levenshtein(test_word, &word), test_word))
+        .fold((usize::max_value(), ""), |(ar, aw), (br, bw)| if br < ar { (br, bw) } else { (ar, aw) });
+        correct_word.into()
+    }
+
     /// Returns all words in the Lexicon that are at most
     /// two edits away from the given word.
     pub fn corrections_for(&self, word: &str) -> Vec<String> {
@@ -50,7 +67,8 @@ impl Lexicon {
             perms.extend_from_slice(Lexicon::generate_permutations(perm).as_slice()); 
         }
 
-        let mut corrections = HashSet::new();
+        //let mut corrections = HashSet::new();
+        let mut corrections: HashSet<String, FnvBuildHasher> = FnvHashSet::default();
         for perm in perms.iter() {
             let matches = self.words.all_matches(perm);
             for m in matches.into_iter() {
@@ -64,18 +82,19 @@ impl Lexicon {
     /// Generates all words that are 1 edit away from the given word.
     /// Wildcards are used to minimize the number of permutations generated.
     fn generate_permutations(word: &str) -> Vec<String> {
-        let mut perms = HashSet::new();
+        //let mut perms = HashSet::new();
+        let mut perms: HashSet<String, FnvBuildHasher> = FnvHashSet::default();
         
         // insertions
         for i in 0..word.len() {
-            let mut string = word.to_string();
+            let mut string: String = word.into();
             string.insert(i, '*');
             perms.insert(string);
         }
 
         // replacements
         for i in 0..word.len() {
-            let mut string = word[..i].to_string();
+            let mut string: String = word[..i].into();
             string.push('*');
             string.push_str(&word[i+1..word.len()]);
             perms.insert(string);
@@ -83,7 +102,7 @@ impl Lexicon {
         
         // deletions
         for i in 0..word.len() {
-            let mut string = word[..i].to_string();
+            let mut string: String = word[..i].into();
             string.push_str(&word[i+1..word.len()]);
             perms.insert(string);
         }
